c6361dc1f5ac6c8b435f22af5d5aac41e5551cec
unmovingcastle <jasonyao8742@icloud.com>
2023-06-03 16:38:47 +0800

    added Xmacros directory

--- current_work/Evolutionary_loop/Loop_Scripts/Asym_XF_Loop.sh
+++ current_work/Evolutionary_loop/Loop_Scripts/Asym_XF_Loop.sh
@@ -18,61 +18,62 @@
 #make sure we're using python3
 module load python/3.6-conda5.2
 
-####### VARIABLES: LINES TO CHECK OVER WHEN STARTING A NEW RUN ###############################################################################################
+####### VARIABLES: LINES TO CHECK OVER WHEN STARTING A NEW RUN ###############################
 RunName='2023_02_20_Symmetric_Run'	## This is the name of the run. You need to make a unique name each time you run.
-TotalGens=100			## number of generations (after initial) to run through
+TotalGens=100		## number of generations (after initial) to run through
 NPOP=50				## number of individuals per generation; please keep this value below 99
-Seeds=10			## This is how many AraSim jobs will run for each individual## the number frequencies being iterated over in XF (Currectly only affects the output.xmacro loop)
+Seeds=10			## This is how many AraSim jobs will run for each individual
 FREQ=60				## the number frequencies being iterated over in XF (Currectly only affects the output.xmacro loop)
 NNT=30000			## Number of Neutrinos Thrown in AraSim   
 exp=18				## exponent of the energy for the neutrinos in AraSim
-ScaleFactor=1.0			## ScaleFactor used when punishing fitness scores of antennae larger than the drilling holes
+ScaleFactor=1.0		## ScaleFactor used when punishing fitness scores of antennae larger than the drilling holes
 GeoFactor=1			## This is the number by which we are scaling DOWN our antennas. This is passed to many files
 num_keys=4			## how many XF keys we are letting this run use
-database_flag=0			## 0 if not using the database, 1 if using the database
+database_flag=0		## 0 if not using the database, 1 if using the database
 				## These next 3 define the symmetry of the cones.
 RADIUS=0			## If 1, radius is asymmetric. If 0, radius is symmetric		
 LENGTH=0			## If 1, length is asymmetric. If 0, length is symmetric
 ANGLE=0				## If 1, angle is asymmetric. If 0, angle is symmetric
 CURVED=0			## If 1, evolve curved sides. If 0, sides are straight
-A=0				## If 1, A is asymmetric
-B=0				## If 1, B is asymmetric
-SEPARATION=0    		## If 1, separation evolves. If 0, separation is constant
-NSECTIONS=1 			## The number of chromosomes
-DEBUG_MODE=0			## 1 for testing (ex: send specific seeds), 0 for real runs
+A=0					## If 1, A is asymmetric (A x^2 + B describes the curve of sides; Jason Yao May31, 2023)
+B=0					## If 1, B is asymmetric
+SEPARATION=0    	## If 1, separation evolves. If 0, separation is constant
+NSECTIONS=1 		## The number of chromosomes
+DEBUG_MODE=0		## 1 for testing (ex: send specific seeds), 0 for real runs
 				## These next variables are the values passed to the GA
-REPRODUCTION=3			## Number (not fraction!) of individuals formed through reproduction
-CROSSOVER=36			## Number (not fraction!) of individuals formed through crossover
+REPRODUCTION=3		## Number (not fraction!) of individuals formed through reproduction
+CROSSOVER=36		## Number (not fraction!) of individuals formed through crossover
 MUTATION=25			## Probability of mutation (divided by 100)
 SIGMA=6				## Standard deviation for the mutation operation (divided by 100)
 ROULETTE=8			## Percent of individuals selected through roulette (divided by 10)
-TOURNAMENT=2			## Percent of individuals selected through tournament (divided by 10)
+TOURNAMENT=2		## Percent of individuals selected through tournament (divided by 10)
 RANK=0				## Percent of individuals selected through rank (divided by 10)
 ELITE=0				## Elite function on/off (1/0)
 
-#####################################################################################################################################################
+##############################################################################################
 
 
-########  INITIALIZATION OF DIRECTORIES  ###############################################################################################################
+########  INITIALIZATION OF DIRECTORIES  #####################################################
 BEOSC=/fs/ess/PAS1960/BiconeEvolutionOSC
-WorkingDir=`pwd` ## this is where the loop is; on OSC this is /fs/ess/PAS1960/BiconeEvolutionOSC/BiconeEvolution/current_antenna_evo_build_XF_Loop/Evolutionary_Loop
-echo $WorkingDir
+# The following is where the loop is; on OSC this is 
+# /fs/ess/PAS1960/BiconeEvolutionOSC/BiconeEvolution/current_antenna_evo_build_XF_Loop/Evolutionary_Loop
+WorkingDir=`pwd`; echo $WorkingDir
 XmacrosDir=$WorkingDir/../Xmacros 
-XFProj=$WorkingDir/Run_Outputs/${RunName}/${RunName}.xf  ## Provide path to the project directory in the 'single quotes'
-echo $XFProj
+XFProj=$WorkingDir/Run_Outputs/${RunName}/${RunName}.xf; echo $XFProj
 #AraSimExec="/fs/ess/PAS1960/BiconeEvolutionOSC/AraSim"  ##Location of AraSim.exe
-AraSimExec=${WorkingDir}/../../../../Original_GENETIS_AraSim/AraSim #"${WorkingDir}/../../../../AraSim" #$BEOSC/AraSim ## Location of AraSim directory
+#"${WorkingDir}/../../../../AraSim" #$BEOSC/AraSim ## Location of AraSim directory
+AraSimExec=${WorkingDir}/../../../../Original_GENETIS_AraSim/AraSim 
 ##Source araenv.sh for AraSim libraries##
 #source /fs/ess/PAS1960/BiconeEvolutionOSC/araenv.sh
 source $WorkingDir/../../../../araenv.sh
 source /fs/ess/PAS1960/BiconeEvolutionOSC/new_root/new_root_setup.sh
 source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh
-#####################################################################################################################################################
+##############################################################################################
 
 
 
 
-######## SAVE STATE #################################################################################################################################
+######## SAVE STATE ##########################################################################
 ##Check if saveState exists and if not then create one at 0,0
 saveStateFile="${RunName}.savestate.txt"
 
@@ -97,39 +98,32 @@ while read p; do
 	if [ $line -eq 1 ]
 	then
 		InitialGen=$p 
-
 	fi
 	
 	if [ $line -eq 2 ]
 	then
 		state=$p
-	        
 	fi
 	
 	if [ $line -eq 3 ]
 	then
 	        indiv=$p
-		
 	fi
 	
 	if [ $line -eq 2 ]
 	then
 		line=3
-
 	fi
 
 	if [ $line -eq 1 ]
 	then
 		line=2
-
 	fi
-
-       	
 done <saveStates/$saveStateFile ## this outputs to the state of the loop to the savestate file
-###########################################################################################################################################
+##############################################################################################
 
 
-##################### THE LOOP ###########################################################################################################
+##################### THE LOOP ###############################################################
 echo "${InitialGen}"
 echo "${state}"
 echo "${indiv}"
@@ -138,13 +132,11 @@ echo "${indiv}"
 
 for gen in `seq $InitialGen $TotalGens`
 do
-	#read -p "Starting generation ${gen} at location ${state}. Press any key to continue... " -n1 -s
-	
 
 	## This only runs if starting new run ##
 	if [[ $gen -eq 0 && $state -eq 0 ]]
 	then
-	        read -p "Starting generation ${gen} at location ${state}. Press any key to continue... " -n1 -s
+    read -p "Starting generation ${gen} at location ${state}. Press any key to continue... " -n1 -s
 		# Make the run name directory
 		mkdir -m777 $WorkingDir/Run_Outputs/$RunName
 		mkdir -m777 $WorkingDir/Run_Outputs/$RunName/AraSimFlags
@@ -179,7 +171,6 @@ do
 	 	
 		state=2
 		./SaveState_Prototype.sh $gen $state $RunName $indiv
-
 	fi
 
 
