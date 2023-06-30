79fac939c45454427faf01dabf52f53c0517c8db
unmovingcastle <jasonyao8742@icloud.com>
2023-06-30 14:13:24 +0800

    Jun30 log

--- current_work/Evolutionary_loop/SaveState_Prototype.sh
+++
@@ -1,7 +0,0 @@
-gen=$1
-state=$2
-runName=$3
-indiv=$4
-echo ${gen} > saveStates/$runName.savestate.txt
-echo ${state} >> saveStates/$runName.savestate.txt 
-echo ${indiv} >> saveStates/$runName.savestate.txt
